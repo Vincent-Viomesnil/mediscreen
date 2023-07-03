8e5d7a312ed9e58b2b7c59a203ead705456b9b1b
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2023-07-03 11:40:48 +0200

    Docker ok

--- mediscreen_patient/src/main/java/com/ocr/mediscreen/MediscreenApplication.java
+++ mediscreen_patient/src/main/java/com/ocr/mediscreen/MediscreenApplication.java
@@ -9,8 +9,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class MediscreenApplication {
 
-	//Spring
-
 	public static void main(String[] args) {
 		SpringApplication.run(MediscreenApplication.class, args);
 	}
